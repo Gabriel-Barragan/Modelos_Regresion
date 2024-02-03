db41c4c149e99d2fd4264548aa6fab964bd6a46d
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-02 21:55:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from streamlit_option_menu import option_menu
 
 st.title('Regresión lineal')
 st.markdown('*Autor: Gabriel Barragán*')
@@ -20,7 +21,7 @@ selected_dataset = st.selectbox("Seleccione una base de datos", datasets)
 # Read the selected dataset into a pandas Dataframe
 df = pd.read_csv('Datasets/'+selected_dataset)
 
-selected = option_menu(None, ["Mostrar base de datos", 'Mostrar diagrama de dispersión', 'Calcular regresión lineal'])
+selected = option_menu(None, ["Mostrar base de datos", "Mostrar diagrama de dispersión", "Calcular regresión lineal"])
 selected
                        
 # Display the Dataframe
