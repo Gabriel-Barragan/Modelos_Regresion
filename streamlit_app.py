afb8f0a89f4fa49879221b3e974afaf3d1e85fb9
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-02 21:02:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 st.header('Regresión lineal')
 st.markdown('*Autor: Gabriel Barragán*')
 
-st.header('Cargar base de datos')
+st.write('# Cargar base de datos')
 
 # Load Data
 # Create a list of datasets
@@ -30,32 +30,37 @@ X = df.iloc[:,0]
 y = df.iloc[:,1]
 
 # Plot a scatterplot
-plt.subplots()
-plt.title('Diagrama de dispersión')
-plt.scatter(X,y)
-plt.xlabel(X.name)
-plt.ylabel(y.name)
-# Display the plot in Streamlit
-st.pyplot(plt)
+if st.checkbox('Mostrar diagrama de dispersión'):
+           st.write('# Diagrama de dispersión')
+           plt.subplots()
+           plt.title('Diagrama de dispersión')
+           plt.scatter(X,y)
+           plt.xlabel(X.name)
+           plt.ylabel(y.name)
+           # Display the plot in Streamlit
+           st.pyplot(plt)
+
 
 # Create a linear regression model
-model = LinearRegression()
+if st.button('Calcular regresión lineal'):
+           st.write('# Diagrama de dispersión')
+           model = LinearRegression()
 
-# Fit the model to the data
-model.fit(X.values.reshape(-1,1), y)
+           # Fit the model to the data
+           model.fit(X.values.reshape(-1,1), y)
 
-intercept = model.intercept_
-coefficient = model.coef_[0]
-st.write(f'Modelo de regressión lineal: y = {coefficient:.4f}x + {intercept:.4f}')
+           intercept = model.intercept_
+           coefficient = model.coef_[0]
+           st.write(f'Modelo de regressión lineal: y = {coefficient:.4f}x + {intercept:.4f}')
 
-# Plot the regression line
-x_vals = X.values.reshape(-1,1)
-y_vals = model.predict(x_vals)
-plt.subplots()
-plt.title('Diagrama de dispersión y recta de regresión')
-plt.scatter(X, y)
-plt.plot(x_vals, y_vals, color='red')
-plt.xlabel(X.name)
-plt.ylabel(y.name)
-# Display the plot in Streamlit
-st.pyplot(plt)
+           # Plot the regression line
+           x_vals = X.values.reshape(-1,1)
+           y_vals = model.predict(x_vals)
+           plt.subplots()
+           plt.title('Diagrama de dispersión y recta de regresión')
+           plt.scatter(X, y)
+           plt.plot(x_vals, y_vals, color='red')
+           plt.xlabel(X.name)
+           plt.ylabel(y.name)
+           # Display the plot in Streamlit
+           st.pyplot(plt)
