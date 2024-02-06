14380ee9e28a5839df95a74b31dc4118198df374
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-06 18:28:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,170 +9,162 @@ from sklearn.linear_model import LinearRegression
 st.title('Regresión lineal')
 st.markdown('*Autor: Gabriel Barragán*')
 
-st.write('# Cargar base de datos')
+tab_titles = ['Regresión lineal','Regresión exponencial','Otros']
+tabs = st.tabs(tab_titles)
 
-# Load Data
-# Create a list of datasets
-datasets = ["1.longitud_femur_estatura.csv",
-            "2.Demanda_bebidas_gaseosas.csv",
-            "3.Diametro_arbol.csv",
-            "4.Nivel_CO2.csv",
-            "5.Temperatura_grillos.csv",
-            "6.extension_hielo.csv",
-            "7.Prevalencia_mosquitos.csv",
-            "8.Ruido_inteligibilidad.csv",
-            "9.Esperanza_vida.csv",
-            "11.Record_Olimpico.csv"
-           ]
+with tabs[0]:
+  st.write('# Cargar base de datos')
 
-# Create a dropdown menu to select the dataset
-selected_dataset = st.selectbox("Seleccione una base de datos", datasets)
+  # Load Data
+  # Create a list of datasets
+  datasets = ["1.longitud_femur_estatura.csv",
+              "2.Demanda_bebidas_gaseosas.csv",
+              "3.Diametro_arbol.csv",
+              "4.Nivel_CO2.csv",
+              "5.Temperatura_grillos.csv",
+              "6.extension_hielo.csv",
+              "7.Prevalencia_mosquitos.csv",
+              "8.Ruido_inteligibilidad.csv",
+              "9.Esperanza_vida.csv",
+              "11.Record_Olimpico.csv"
+              ]
 
-# Read the selected dataset into a pandas Dataframe
-df = pd.read_csv('Datasets/'+selected_dataset)
+  # Create a dropdown menu to select the dataset
+  selected_dataset = st.selectbox("Seleccione una base de datos", datasets)
 
-if selected_dataset != '11.Record_Olimpico.csv':
-            st.write('# Datos no agrupados')
+  # Read the selected dataset into a pandas Dataframe
+  df = pd.read_csv('Datasets/'+selected_dataset)
 
-            # Display the Dataframe
-            if st.checkbox('Mostrar base de datos'):
-                       st.write('Base de datos: '+selected_dataset)
-                       st.dataframe(df)
+  if selected_dataset != '11.Record_Olimpico.csv':
+    st.write('# Datos no agrupados')
 
-            if st.checkbox('Mostrar estadísticos descriptivos'):
-                        st.write(df.describe())
-                        
-                        correlation_coef = df[df.columns[0]].corr(df[df.columns[1]])
-                        st.write(f'Coeficiente de correlación entre la variable {df.columns[0]} y {df.columns[1]}: R = {correlation_coef:.2f}')
+    # Display the Dataframe
+    if st.checkbox('Mostrar base de datos'):
+      st.write('Base de datos: '+selected_dataset)
+      st.dataframe(df)
+
+    if st.checkbox('Mostrar estadísticos descriptivos'):
+      st.write(df.describe())
+      correlation_coef = df[df.columns[0]].corr(df[df.columns[1]])
+      st.write(f'Coeficiente de correlación entre la variable {df.columns[0]} y {df.columns[1]}: R = {correlation_coef:.2f}')
+      
+      # Access X and y variables
+      X = df.iloc[:,0]
+      y = df.iloc[:,1]
+
+      #if st.checkbox('Visualización de variable de respuesta'):
+      #           st.write(f'# Visualización de {y.name}') 
+      #           input_bins = st.slider('Ingrese número de bins', 1, 20, 5)
+      #           plt.subplots()
+      #           plt.title(f'Histograma de {y.name}') 
+      #           plt.hist(x=y, bins=input_bins, edgecolor='black') 
+      #           plt.xlabel('Valor')
+      #           plt.ylabel('Frecuencia')
+      #           st.pyplot(plt)
+
+      #          plt.subplots()
+      #          plt.title(f'Boxplot de {y.name}')
+      #          plt.boxplot(y) 
+      #          st.pyplot(plt)
+  
+      #          plt.subplots()
+      #          #plt.title(f'Q-Q plot de  {y.name}')
+      #          stats.probplot(y, dist='norm', plot=plt) 
+      #          st.pyplot(plt) 
             
-            # Access X and y variables
-            X = df.iloc[:,0]
-            y = df.iloc[:,1]
+      # Plot a scatterplot
+      if st.checkbox('Mostrar diagrama de dispersión'):
+        st.write('# Diagrama de dispersión')
+        plt.subplots()
+        plt.title('Diagrama de dispersión')
+        plt.scatter(X,y)
+        plt.xlabel(X.name)
+        plt.ylabel(y.name)
+        # Display the plot in Streamlit
+        st.pyplot(plt)
 
-            #if st.checkbox('Visualización de variable de respuesta'):
-            #           st.write(f'# Visualización de {y.name}') 
-            #           input_bins = st.slider('Ingrese número de bins', 1, 20, 5)
-            #           plt.subplots()
-            #           plt.title(f'Histograma de {y.name}') 
-            #           plt.hist(x=y, bins=input_bins, edgecolor='black') 
-            #          plt.xlabel('Valor')
-            #           plt.ylabel('Frecuencia')
-            #          st.pyplot(plt)
+      # Create a linear regression model
+      if st.checkbox('Calcular regresión lineal'):
+        st.write('# Modelo de regresión lineal')
+        model = LinearRegression()
 
-            #          plt.subplots()
-            #          plt.title(f'Boxplot de {y.name}')
-            #          plt.boxplot(y) 
-            #          st.pyplot(plt)
-                        
-            #          plt.subplots()
-            #          #plt.title(f'Q-Q plot de  {y.name}')
-            #          stats.probplot(y, dist='norm', plot=plt) 
-            #          st.pyplot(plt) 
-            
-            # Plot a scatterplot
-            if st.checkbox('Mostrar diagrama de dispersión'):
-                       st.write('# Diagrama de dispersión')
-                       plt.subplots()
-                       plt.title('Diagrama de dispersión')
-                       plt.scatter(X,y)
-                       plt.xlabel(X.name)
-                       plt.ylabel(y.name)
-                       # Display the plot in Streamlit
-                       st.pyplot(plt)
-
-            # Create a linear regression model
-            if st.checkbox('Calcular regresión lineal'):
-                       st.write('# Modelo de regresión lineal')
-                       model = LinearRegression()
-
-                       # Fit the model to the data
-                       model.fit(X.values.reshape(-1,1), y)
-
-                       intercept = model.intercept_
-                       coefficient = model.coef_[0]
-                       st.write('x: ',X.name)
-                       st.write('y: ',y.name)
-                       st.write(f'Modelo de regresión lineal: y = {coefficient:.4f}x + {intercept:.4f}')
+        # Fit the model to the data
+        model.fit(X.values.reshape(-1,1), y)
+        intercept = model.intercept_
+        coefficient = model.coef_[0]
+        st.write('x: ',X.name)
+        st.write('y: ',y.name)
+        st.write(f'Modelo de regresión lineal: y = {coefficient:.4f}x + {intercept:.4f}')
                                   
-                       # Plot the regression line
-                       #x_vals = X.values.reshape(-1,1)
-                       #y_vals = model.predict(x_vals)
-                       #plt.subplots()
-                       #plt.title('Diagrama de dispersión y recta de regresión')
-                       #plt.scatter(X, y)
-                       #plt.plot(x_vals, y_vals, color='red')
-                       #plt.xlabel(X.name)
-                       #plt.ylabel(y.name)
-                       # Display the plot in Streamlit
-                       #st.pyplot(plt)
+        x_min = st.number_input('Valor mínimo x:',value=X.min())
+        x_max = st.number_input('Valor máximo x:',value=X.max()) 
+        x_range_prediction = np.arange((0.99)*x_min,(1.01)*x_max,1)
+        y_range_prediction = model.predict(x_range_prediction.reshape(-1,1))
+         
+        plt.subplots()
+        plt.title('Diagrama de dispersión y recta de regresión')
+        plt.scatter(X, y)
+        plt.plot(x_range_prediction, y_range_prediction, color='red')
+        plt.xlabel(X.name)
+        plt.ylabel(y.name)
+        # Display the plot in Streamlit
+        st.pyplot(plt)
+
+         # Predict a new value
+        st.write('# Predicción de valores con el modelo de regresión lineal')
+        st.write('x: ',X.name)
+        input_value = st.number_input('Introduce un valor de x', value=X.min())
+
+        predicted_value = model.predict([[input_value]])
+        st.write(f'Si {X.name} es {input_value} entonces {y.name} es {predicted_value[0]:.2f}')
+  
+  else: 
+    st.write('# Datos agrupados')
+    grouped_data = df.groupby('Sexo')
+    # Display the Dataframe
+    if st.checkbox('Mostrar base de datos'):
+      st.write('Base de datos: '+selected_dataset)
+      st.dataframe(df)
             
-                       x_min = st.number_input('Valor mínimo x:',value=X.min())
-                       x_max = st.number_input('Valor máximo x:',value=X.max())
-                       x_range_prediction = np.arange((0.99)*x_min,(1.01)*x_max,1)
-                       y_range_prediction = model.predict(x_range_prediction.reshape(-1,1))
-                       plt.subplots()
-                       plt.title('Diagrama de dispersión y recta de regresión')
-                       plt.scatter(X, y)
-                       plt.plot(x_range_prediction, y_range_prediction, color='red')
-                       plt.xlabel(X.name)
-                       plt.ylabel(y.name)
-                       # Display the plot in Streamlit
-                       st.pyplot(plt)
-
-                       # Predict a new value
-                       st.write('# Predicción de valores con el modelo de regresión lineal')
-                       st.write('x: ',X.name)
-                       input_value = st.number_input('Introduce un valor de x', value=X.min())
-
-                       predicted_value = model.predict([[input_value]])
-                       st.write(f'Si {X.name} es {input_value} entonces {y.name} es {predicted_value[0]:.2f}')
-else: 
-            st.write('# Datos agrupados')
-            grouped_data = df.groupby('Sexo')
-            # Display the Dataframe
-            if st.checkbox('Mostrar base de datos'):
-                       st.write('Base de datos: '+selected_dataset)
-                       st.dataframe(df)
-            
-            if st.checkbox('Mostrar estadísticos descriptivos'):
-                        st.write(grouped_data['Tiempo'].describe())
-
-                        for name, group in grouped_data:
-                                    st.write(f"Coeficiente de correlación ({name}): R = {group['Anio'].corr(group['Tiempo']):.2f}")
+    if st.checkbox('Mostrar estadísticos descriptivos'):
+      st.write(grouped_data['Tiempo'].describe())
+      
+      for name, group in grouped_data:
+        st.write(f"Coeficiente de correlación ({name}): R = {group['Anio'].corr(group['Tiempo']):.2f}")
                         
-                        # Bar chart of number of athletes per country, grouped by sex
-                        plt.subplots()
-                        plt.title('Número de atletas por país agrupados por sexo')
-                        sns.histplot(data=df, y="Pais", hue="Sexo", stat="count", multiple="dodge", shrink=0.75)
-                        st.pyplot(plt)
-
+        # Bar chart of number of athletes per country, grouped by sex
+      plt.subplots()
+      plt.title('Número de atletas por país agrupados por sexo')
+      sns.histplot(data=df, y="Pais", hue="Sexo", stat="count", multiple="dodge", shrink=0.75)
+      st.pyplot(plt)
+      
+      if st.checkbox('Mostrar diagrama de dispersión'):
+        st.write('# Diagrama de dispersión')
+        plt.subplots()
+        plt.title('Diagrama de dispersión')
+        sns.scatterplot(data=df, x="Anio", y="Tiempo", hue="Sexo")
+        st.pyplot(plt)
+      
+      # Create a linear regression model
+      if st.checkbox('Calcular regresión lineal'):
+        st.write('# Modelo de regresión lineal')
+        models = {}
+        plt.subplots()
+        for name, group in grouped_data:
+          model = LinearRegression()
+          model.fit(group['Anio'].values.reshape(-1,1), group['Tiempo'])
+          models[name] = model
+          st.write(f"Modelo de regresión lineal ({name}): y= {model.coef_[0]:.4f}x + {model.intercept_:.4f}")
+          plt.scatter(group['Anio'], group['Tiempo'], label=name)
+          x_vals = np.linspace(group['Anio'].min(), (1.025)*group['Anio'].max())
+          y_vals = models[name].predict(x_vals.reshape(-1,1))
+          plt.plot(x_vals, y_vals, label=f"Recta regresión: {name}")
             
-            if st.checkbox('Mostrar diagrama de dispersión'):
-                        st.write('# Diagrama de dispersión')
-                        plt.subplots()
-                        plt.title('Diagrama de dispersión')
-                        sns.scatterplot(data=df, x="Anio", y="Tiempo", hue="Sexo")
-                        st.pyplot(plt)
+        plt.xlabel("Anio")
+        plt.ylabel("Tiempo")
+        plt.title("Diagrama de dispersión y recta de regresión por grupos")
+        plt.legend()
+        st.pyplot(plt)
 
-            # Create a linear regression model
-            if st.checkbox('Calcular regresión lineal'):
-                        st.write('# Modelo de regresión lineal')
-                        
-                        models = {}
-                        
-                        plt.subplots()
-                        for name, group in grouped_data:
-                                    model = LinearRegression()
-                                    model.fit(group['Anio'].values.reshape(-1,1), group['Tiempo'])
-                                    models[name] = model
-                                    st.write(f"Modelo de regresión lineal ({name}): y= {model.coef_[0]:.4f}x + {model.intercept_:.4f}")
-                                    plt.scatter(group['Anio'], group['Tiempo'], label=name)
-                                    x_vals = np.linspace(group['Anio'].min(), (1.025)*group['Anio'].max())
-                                    y_vals = models[name].predict(x_vals.reshape(-1,1))
-                                    plt.plot(x_vals, y_vals, label=f"Recta regresión: {name}")
-            
-                        plt.xlabel("Anio")
-                        plt.ylabel("Tiempo")
-                        plt.title("Diagrama de dispersión y recta de regresión por grupos")
-                        plt.legend()
-                        st.pyplot(plt)
+with tabs[1]:
+  st.write('# Cargar base de datos')
