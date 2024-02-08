0a54075ec3cdfd90086fdbcf7595fa5a415a026b
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-07 23:03:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -256,4 +256,4 @@ with tabs[1]:
     C = np.exp(model_exponential.intercept_)
     k = model_exponential.coef_[0]
     
-    st.write(r'= Ce^{kx}'+rf' donde k={k}')
+    st.write(r'''y = Ce^{kx}'''+ rf''' donde {k:.2f} ''')
