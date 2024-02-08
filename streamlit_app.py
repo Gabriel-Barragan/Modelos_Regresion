687caee9c84b46e9b1765a4d6373120006236439
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-07 22:32:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -188,6 +188,7 @@ with tabs[1]:
   selected_dataset_2 = st.selectbox("Seleccione una base de datos", datasets_2)
 
   # Read the selected dataset into a pandas Dataframe
+  df_2 = pd.read_csv('Datasets/Poblacion_Estados_Unidos.csv')
   df_2 = pd.read_csv('Datasets/'+selected_dataset_2+'.csv')
 
   # Display the Dataframe
@@ -205,6 +206,7 @@ with tabs[1]:
   
   st.write('# Seleccionar variables y mostrar')
   columns = df_2.columns.tolist()
+  selected_columns = ['Anio', 'Poblacion']
   selected_columns = st.multiselect('Seleccionar variables', columns)
   filtered_data = df_2[selected_columns]
   st.dataframe(filtered_data.head())
