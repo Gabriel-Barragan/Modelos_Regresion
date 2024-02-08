cc2447fd6bee0543793a41a5335d02265e678424
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-07 23:05:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -256,4 +256,4 @@ with tabs[1]:
     C = np.exp(model_exponential.intercept_)
     k = model_exponential.coef_[0]
     
-    st.latex(r'''y = Ce^{kx}'''+ rf''' donde {k:.2f} ''')
+    st.latex(r'''y = Ce^{kx} '''+ rf''' \text{donde} C={C:.4f} \text{y} k={k:.4f}''')
