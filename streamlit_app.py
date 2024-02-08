4fb93ccdc4903ad71fe7e374ab9eb6224ea53908
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-07 23:18:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -257,4 +257,4 @@ with tabs[1]:
     k = model_exponential.coef_[0]
     
     st.latex(r'''y = Ce^{kx}''')
-    st.latex(rf'''\text{Parámetros:} \quad C={C:.4f},\quad k={k:.4f}''')
+    st.write(f'Parámetros: C = {C:.4f}, y k = {k:.4f}')
