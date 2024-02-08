01255b3144f683498593e879a5fdca9fabf5df39
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-07 22:56:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -256,4 +256,4 @@ with tabs[1]:
     C = np.exp(model_exponential.intercept_)
     k = model_exponential.coef_[0]
     
-    st.latex(r'''\theta_b='''+ rf'''{k} ''')
+    st.latex(r$$y = Ce^{kx}$$ + f'''{k:.2f}''')
