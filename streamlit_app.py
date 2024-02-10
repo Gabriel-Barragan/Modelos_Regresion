60afdc0d89924768ed3ff061f2840afac5327045
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-10 10:23:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -271,9 +271,8 @@ with tabs[1]:
         st.latex(r'''A_{\text{exp}}=\ln(C)=''' + rf'''{log_C:.4f}''' + r'''\quad \Rightarrow \quad C=''' + rf'''{C:.4f}''')
 
         st.write(f"Modelo de regresión exponencial: $$y = {C:.4f}x^{{{k:.4f}}}$$")
-        st.write(f'Coeficiente de determinación:')
-        st.latex(r'''R^2='''+rf'{r2_score(X, log_y):.4f}''')
-
+        st.write(f'Coeficiente de determinación: $$R^2={r2_score(X, log_y):.4f}$$')
+        
         # Predict a new value
         st.write('# Predicción de valores con el modelo de regresión exponencial')
         st.write('x: ',X.name)
